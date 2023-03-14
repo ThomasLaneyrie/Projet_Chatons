d44f24613c15cfdfb697ffd13e19ef60d55c1848
sbellotlecoq <sbellotlecoq@gmail.com>
2023-03-14 15:22:24 +0100

    controlleur avec user qui s'authentifie et panier ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :set_render_cart
+    before_action :initialize_cart
+
+    def set_render_cart
+      @render_cart = true
+    end
+  
+  
+    #if user pas sign in panier libre mais faut se connecter apres
+    #if user connecte lui cree son panier
+
+    def initialize_cart
+      if current_user != nil
+			@cart =	Cart.where(user_id: current_user.id).last
+								
+					if @cart == nil
+						@cart = Cart.create(user_id: current_user.id)
+					end
+			end
+    end
+  
+  end
---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,23 @@
+class CartsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:create]
+
+  def show
+    @cart = Cart.find(params[:id])
+    @render_cart = false
+  end
+
+  def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+  end
+  
+  def create
+        Cart.find_by(user_id: current_user.id)
+             
+        if @cart.nil?
+          @cart = Cart.create(user_id: current_user.id)
+        end		
+  end
+
+end
