d82a8b37b5b27d548c9d738dde1e7660dcffa3e3
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-03-14 18:03:09 +0100

    back_show_panier

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,11 +13,9 @@ class CartsController < ApplicationController
   end
   
   def create
-        Cart.find_by(user_id: current_user.id)
-             
-        if @cart.nil?
-          @cart = Cart.create(user_id: current_user.id)
-        end		
+    Cart.find_by(user_id: current_user.id)   
+    if @cart.nil?
+      @cart = Cart.create(user_id: current_user.id)
+    end		
   end
-
 end
